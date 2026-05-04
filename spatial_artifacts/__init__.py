62cc2c3548d7ebb4a9e63ed3f9d4a0bfd36aebc4
Jiali He <jialihe@umich.edu>
2026-05-03 22:29:19 -0400

    add wrapper, tutorials, MANIFEST.in

--- spatial_artifacts/__init__.py
+++ spatial_artifacts/__init__.py
@@ -1,8 +1,18 @@
 from .detect_visium import detect_edge_artifacts_visium
 from .detect_visiumhd import detect_edge_artifacts_visiumhd
 from .classify import classify_edge_artifacts
- 
+
+def detect_edge_artifacts(adata, platform, **kwargs):
+    platform = platform.lower()
+    if platform == "visium":
+        return detect_edge_artifacts_visium(adata, **kwargs)
+    elif platform == "visiumhd":
+        return detect_edge_artifacts_visiumhd(adata, **kwargs)
+    else:
+        raise ValueError(f"Unknown platform: '{platform}'. Choose 'visium' or 'visiumhd'.")
+
 __all__ = [
+    "detect_edge_artifacts",
     "detect_edge_artifacts_visium",
     "detect_edge_artifacts_visiumhd",
     "classify_edge_artifacts",
